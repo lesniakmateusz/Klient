8688948d331e8eb4828312460ba64023bbf6af4d
Mateusz Lesniak <szlachmetal@gmail.com>
2022-10-23 09:39:36 +0200

    Init commit

--- src/Klient.java
+++ src/Klient.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class Klient {
+public class Klient {
+
+
+    private int id;
+    private String imie;
+    private String nazwisko;
+    private String miejscowosc;
+
+    public Klient(int id,String imie,String nazwisko, String miejscowosc){
+
+        this.id = id;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.miejscowosc = miejscowosc;
+    }
+
+    public double getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getMiejscowosc() {
+        return miejscowosc;
+    }
+
+    public void setMiejscowosc(String miejscowosc) {
+        this.miejscowosc = miejscowosc;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "id=" + id +
+                ", imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                ", miejscowosc='" + miejscowosc;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Podaj id klienta:");
+        int a = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Podaj imie klienta:");
+        String b = sc.nextLine();
+        System.out.println("Podaj nazwisko klienta:");
+        String c = sc.nextLine();
+        System.out.println("Podaj miejscowowsc klienta:");
+        String d = sc.nextLine();
+
+
+
+
+
+
+        Klient klient = new Klient(a,b,c,d);
+
+        System.out.println(klient.toString());
+
+
     }
 }
